9f4edee2ecab3394a45de3c39c25143f873a4150
Baris Macaroglu <barismacaroglu@hotmail.com>
2021-03-01 09:45:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Script chargé !");
+
+const iconFacebook = document.getElementById("facebook");
+const iconTwitter = document.getElementById("twitter");
+const iconInstagram = document.getElementById("instagram");
+
+iconFacebook.onclick = function() {
+    window.alert("Je ne suis pas sur Facebook :)")
+};
+
+iconTwitter.onclick = function() {
+    window.alert("Je ne suis pas sur Twitter :)")
+};
+
+iconInstagram.onclick = function() {
+    window.alert("Je ne suis pas sur Instagram :)")
+};
+
